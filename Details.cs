3db33bd3e9cff5288106c44177de1a2cf27b131e
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-24 19:39:11 +0530

    JSON Inventory Data Management of Rice, Pulses and Wheats

---
+++ Details.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Details
+    {
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfWheet> typesOfWheet;
+        public List<TypesOfPulses> typesOfPulses;
+    }
+
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesOfWheet
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesOfPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
